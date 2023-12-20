173f76db635113b6d39d06ab8a2d4f53ac6beed1
Coconut <coconutat@protonmail.com>
2023-12-20 21:41:56 +0800

    build script:disable DTC_EXT

--- build_kernel_8t.sh
+++ build_kernel_8t.sh
@@ -7,10 +7,11 @@ export CC=clang
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=aarch64-linux-gnu-
 export CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+# export CROSS_COMPILE_COMPAT=arm-linux-gnueabi-
 # export CONFIG_BUILD_ARM64_DT_OVERLAY=y
 
 export ARCH=arm64
-export DTC_EXT=dtc
+# export DTC_EXT=dtc
 
 if [ ! -d "out" ]; then
 	mkdir out
