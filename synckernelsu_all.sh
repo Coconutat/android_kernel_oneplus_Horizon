dfb821bab29d647c9b8217f19f48e375c5bd1833
Coconut <coconutat@protonmail.com>
2025-09-24 19:37:06 +0800

    Sync Script Add SukiSU

--- synckernelsu_all.sh
+++ synckernelsu_all.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ -d KernelSU ]; then
-    echo "Found KernelSU Folder, removing it..."
+    echo "Found KernelSU/SukiSU Folder, removing it..."
     rm -rf KernelSU
 	rm -rf drivers/kernelsu
 else
-    echo "KernelSU Folder not found, proceeding..."
+    echo "KernelSU/SukiSU Folder not found, proceeding..."
 fi
 
-read -p "Please enter the version number (A/a is tags version.B/b is main version.C/c is special v0.9.2 Version For Huawei): " version
+read -p "Please enter the version number (A/a is tags version.B/b is SukiSU non-GKI version.C/c is special v0.9.2 Version For Huawei): " version
 
 if [ "$version" == "" ]; then
     echo "No version specified. Exiting."
@@ -21,7 +21,7 @@ case $version in
         curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v"$specified_version"
         ;;
     [Bb]*)
-        curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s main
+        curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
         ;;
     [Cc]*)
         curl -LSs https://raw.githubusercontent.com/Coconutat/KernelSU_backup/0.9.2_upstream_backup/kernel/setup.sh | bash -s 0.9.2_upstream_backup
